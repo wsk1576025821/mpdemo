7fdbc6d7b173f3d56b22378c4fde57c609eea7ba
wangshuaikang <wangshuaikang@51credit.com>
2020-11-08 18:11:46 +0800

    * 初始化mpvue

---
+++ src/pages/index/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <!-- <div class="btn" @click="handleClick">click me</div> -->
+    111
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+    handleClick(){
+      console.log('111');
+    }
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container{
+  .btn{
+    width: 100px;
+    height: 50px;
+    background: #ccc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
